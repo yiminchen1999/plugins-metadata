c694bb6b9510ef23ea1662542a2c3aed5bb36ec4
ÿiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2023-06-29 16:04:08 -0400

    run

--- vb ver3.py
+++ vb ver3.py
@@ -12,12 +12,14 @@ from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain
 from tqdm.auto import tqdm
 import logging
 from uuid import uuid4
+
 # Configure logging
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     filename='debug.log'
 )
+
 # Load the OpenAI API key from an environment variable
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -29,10 +31,12 @@ tokenizer = tiktoken.get_encoding('cl100k_base')
 # Load the dataset or read the Excel file
 df = pd.read_excel('/Users/chenyimin/PycharmProjects/plugins-quickstart/CSCL_1995_fullcopy.xlsx', engine='openpyxl')
 
+
 # Create the length function
 def tiktoken_len(text):
     tokens = tokenizer.encode(text)
     return len(tokens)
+
 
 # Split the text into chunks
 text_splitter = RecursiveCharacterTextSplitter(
@@ -45,15 +49,17 @@ text_splitter = RecursiveCharacterTextSplitter(
 chunks = text_splitter.split_text(df['content'])[:3]
 print(chunks)
 
-print(tiktoken_len(chunks[0])) # 201
-print(tiktoken_len(chunks[1])) # 198
-print(tiktoken_len(chunks[2])) # 204
+print(tiktoken_len(chunks[0]))  # 201
+print(tiktoken_len(chunks[1]))  # 198
+print(tiktoken_len(chunks[2]))  # 204
 
 model_name = 'text-embedding-ada-002'
+
 
 def embed_query(texts):
     embeddings = embed.embed_documents(texts)
     return embeddings
+
 
 embed = OpenAIEmbeddings(
     model=model_name,
@@ -66,40 +72,35 @@ texts = [
 ]
 
 res = embed_query(texts)
-print(len(res), len(res[0])) # 2 1536
+print(len(res), len(res[0]))  # 2 1536
+
 import pinecone
-# Commented out index creation
+
 # Create a new index
 index_name = 'langchain-retrieval-augmentation'
 
 pinecone.init(
-     api_key=config["PINECONE_API_KEY"],
+    api_key=config["PINECONE_API_KEY"],
     environment=config['PINECONE_ENVIRONMENT']
- )
+)
 
-if index_name not in pinecone.list_indexes():
-    # we create a new index
-    pinecone.create_index(
-        name=index_name,
-        metric='cosine',
-        dimension=len(res[0])  # 1536 dim of text-embedding-ada-002
-    )
 index = pinecone.Index(index_name=index_name)
 
 index.describe_index_stats()
+
 # Check if the index exists
-#if indexes is not None and index_name in indexes:
+if index_name in pinecone.list_indexes():
     # Clear the index
-    #pinecone.delete_index(index_name)
-    #print(f"Index '{index_name}' has been cleared.")
-#else:
-    #print(f"Index '{index_name}' does not exist.")
-
+    pinecone.delete_index(index_name)
+    print(f"Index '{index_name}' has been cleared.")
+else:
+    print(f"Index '{index_name}' does not exist.")
 
 index = pinecone.Index(index_name=index_name)
 index.describe_index_stats()
+
 # Batch upsert operation
-batch_limit = 500
+batch_limit = 100
 
 texts = []
 metadatas = []
@@ -114,28 +115,23 @@ for i, record in enumerate(tqdm(df)):
     # Create chunks
     record_texts = text_splitter.split_text(df['content'])
     # Continued from previous code snippet
-
     record_metadatas = [{
         "chunk": j, "content": content, **metadata
-    }for j, content in enumerate(record_texts)]
+    } for j, content in enumerate(record_texts)]
 
     # Append these to current batches
     texts.extend(record_texts)
     metadatas.extend(record_metadatas)
+
     # If reach batch limit or the last record
-    # if we have reached the batch_limit we can add texts
-    if len(texts) >= batch_limit:
+    if len(texts) >= batch_limit or i == len(df) - 1:
         ids = [str(uuid4()) for _ in range(len(texts))]
         embeds = embed.embed_documents(texts)
         index.upsert(vectors=zip(ids, embeds, metadatas))
         texts = []
         metadatas = []
 
-if len(texts) > 0:
-    ids = [str(uuid4()) for _ in range(len(texts))]
-    embeds = embed.embed_documents(texts)
-    index.upsert(vectors=zip(ids, embeds, metadatas))
-
 # Verify the upserted items
 index.describe_index_stats()
+
 
