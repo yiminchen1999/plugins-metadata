516d63a87c6b00930b67b11301f0d866bb3125d5
ÿiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2023-07-24 21:45:07 -0400

    run

--- right match the file.py
+++ right match the file.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Excel file
-df = pd.read_excel('ISLS_1995-2021/CSCL_2005.xlsx')
+df = pd.read_excel('ISLS_1995-2021/CSCL_2011.xlsx')
 
 # Rename the column to 'id'
 df.rename(columns={'dc.identifier.uri[]': 'id'}, inplace=True)
@@ -13,7 +13,7 @@ new_df = df.copy()
 new_df['id'] = new_df['id'].apply(lambda uri: str(uri).split('.')[-1])
 
 # Save the modified DataFrame back to the Excel file
-new_df.to_excel('CSCL_2005_revised_11.xlsx', index=False)
+new_df.to_excel('CSCL_2011_revised_11.xlsx', index=False)
 
 print("Files saved with renamed column.")
 
